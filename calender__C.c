a1242fb8273835c39e964227a18461aed21b7d7e
Raghav Khandelwal <60818611+Zeigler88@users.noreply.github.com>
2020-10-17 18:56:38 +0530

    Add files via upload

---
+++ calender__C.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct bank{
+int acc_no;
+char name[100];
+int balance;}s[100];
+int main()
+{
+    int i;
+
+    for(i=0;i<10;i++)
+    {
+        printf("enter account number");
+        scanf("%d",&s.acc_no[i]);
+        printf("enter name");
+        scanf("%s",&s.name[i]);
+        printf("enter balance");
+        scanf("%d",&s.balance[i]);
+    }
+    if()
+
+
+
+}
